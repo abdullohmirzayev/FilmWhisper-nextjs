b01ecccbee5ed880ee3a85ec2594bfd1bf3f0e65
Abdulloh <abdullohmirzayev7570@gmail.com>
2024-02-15 14:49:02 +0500

    added #11. Carousel

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,5 +9,9 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("tailwindcss-textshadow")],
+  plugins: [
+    require("tailwindcss-textshadow"),
+    require("tailwind-scrollbar-hide"),
+    require("tailwind-scrollbar"),
+  ],
 };
