b2634d1124b6597610be7f83581388dfd90ea2e7
Abdulloh <abdullohmirzayev7570@gmail.com>
2024-02-16 11:54:54 +0500

    added #13. Firebase

---
+++ src/firebase/index.ts
@@ -0,0 +1,19 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAgJv83KGCZWhA7lONfbuQj5Ly8YtFH5Hk",
+  authDomain: "film-whisper-movie.firebaseapp.com",
+  projectId: "film-whisper-movie",
+  storageBucket: "film-whisper-movie.appspot.com",
+  messagingSenderId: "556025193234",
+  appId: "1:556025193234:web:eb2757036cf71c4ff4c9cb",
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const auth = getAuth();
+
+export default app;
+export { db, auth };
