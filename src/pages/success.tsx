f53a25fd7652627489ed35527d1125b1c54953b3
Abdulloh <abdullohmirzayev7570@gmail.com>
2024-02-19 15:12:46 +0500

    added Success page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
+import { AiFillCheckCircle } from "react-icons/ai";
+import { BiErrorAlt } from "react-icons/bi";
 
 const Success = () => {
   return (
     <>
-      <div className="flex justify-start">
+      <div className="flex justify-start py-2 px-4">
         <Image
           src={"/logo.svg"}
           alt={"logo"}
@@ -11,6 +14,15 @@ const Success = () => {
           height={56}
           className={"cursor-pointer object-contain"}
         />
+      </div>
+      <div className="h-[90vh] flex flex-col justify-center items-center">
+        <AiFillCheckCircle className="w-20 h-20 text-green-500" />
+        <h1 className="text-2xl md:text-5xl mt-4">Subscription Completed</h1>
+        <Link href={"/"}>
+          <button className="mt-4 bg-green-500 hover:bg-green-400 py-4 px-6 rounded">
+            Dashboard
+          </button>
+        </Link>
       </div>
     </>
   );
